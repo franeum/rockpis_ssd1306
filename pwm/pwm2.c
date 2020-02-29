4214484cd54bf80a2c22c24cce246398355b834c
franeum <elettrochuck@gmail.com>
2020-02-29 23:12:32 +0000

    fix pwm

--- pwm/pwm2.c
+++ pwm/pwm2.c
@@ -6,31 +6,27 @@
 /* mraa header */
 #include "mraa/pwm.h"
 
-/* gpio declaration */
-// #define GPIO_PIN_1 22
-
 /* PWM declaration */
-#define PWM 3
+#define PWM 13
 
 /* PWM period in us */
 #define PWM_FREQ 200
 
-static t_class *pwm_class;  
+static t_class *pwm2_class;  
  
-typedef struct _pwm {  
+typedef struct _pwm2 {  
   	t_object x_obj;
 	volatile sig_atomic_t flag;
 	int state;
  	mraa_result_t status;
-    	//mraa_gpio_context gpio_1;
-	mraa_pwm_context pwm;
-} t_pwm;  
+	mraa_pwm_context pwm2;
+} t_pwm2;  
 
 
 
 /* gestisce l'uscita */
 
-void sig_handler(t_pwm *x, int signum)
+void sig_handler(t_pwm2 *x, int signum)
 {
     if (signum == SIGINT) {
         fprintf(stdout, "Exiting...\n");
@@ -38,28 +34,34 @@ void sig_handler(t_pwm *x, int signum)
     }
 }
 
-void pwm_free(t_pwm *x)
+void pwm2_free(t_pwm2 *x)
 {
 
     /* release gpio's */
-    x->status = mraa_pwm_close(x->pwm);
+    x->status = mraa_pwm_close(x->pwm2);
 
     //! [Interesting]
     /* deinitialize mraa for the platform (not needed most of the times) */
     mraa_deinit();
 }
 
-void pwm_float(t_pwm *x, t_floatarg f)  
+void pwm2_float(t_pwm2 *x, t_floatarg f)  
 {
   //(void)x; // silence unused variable warning
   //x->state = 1 - x->state;
-  post("%d", f);
-  x->status = mraa_pwm_write(x->pwm, f);
+  post("%f", f);
+  x->status = mraa_pwm_write(x->pwm2, f);
 }
 
-void *pwm_new(void)  
+void pwm2_bang(t_pwm2 *x)
+{
+  post("bang");
+  x->status = mraa_pwm_write(x->pwm2, 0.95);
+}
+
+void *pwm2_new(void)  
 {  
-  t_pwm *x = (t_pwm *)pd_new(pwm_class);  
+  t_pwm2 *x = (t_pwm2 *)pd_new(pwm2_class);  
   x->flag = 1;
   x->state = 0;
 
@@ -70,13 +72,13 @@ void *pwm_new(void)
 
     /* initialize mraa for the platform (not needed most of the times) */
     mraa_init();
-    x->pwm = mraa_pwm_init(PWM);
+    x->pwm2 = mraa_pwm_init(PWM);
     //! [Interesting]
     /* initialize GPIO pin */
     //x->gpio_1 = mraa_gpio_init(GPIO_PIN_1);
-    x->status = mraa_pwm_period_us(x->pwm, PWM_FREQ);
+    x->status = mraa_pwm_period_us(x->pwm2, PWM_FREQ);
     
-    x->status = mraa_pwm_enable(x->pwm, 1);
+    x->status = mraa_pwm_enable(x->pwm2, 1);
     
     
     /* set GPIO to output */
@@ -86,9 +88,10 @@ void *pwm_new(void)
   return (void *)x;  
 }  
  
-void pwm_setup(void) {  
-  pwm_class = class_new(gensym("pwm"),  
-    (t_newmethod)pwm_new, NULL,
-    sizeof(t_pwm), CLASS_DEFAULT, 0);  
-  class_addfloat(pwm_class, pwm_float);  
+void pwm2_setup(void) {  
+  pwm2_class = class_new(gensym("pwm2"),  
+    (t_newmethod)pwm2_new, NULL,
+    sizeof(t_pwm2), CLASS_DEFAULT, 0);  
+  class_addfloat(pwm2_class, pwm2_float);  
+  class_addbang(pwm2_class, pwm2_bang);
 }
