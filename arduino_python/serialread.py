e99992d4247f27e75f43d9864e04feeb79c3df8f
neum <neum@mydebian.station>
2020-06-13 17:10:53 +0200

    add liblo OSC control

--- arduino_python/serialread.py
+++ arduino_python/serialread.py
@@ -2,16 +2,33 @@
 
 import serial
 import time
+import liblo 
 
-arduino = serial.Serial('/dev/ttyS0', 9600, timeout=None)
+def send_msg(target, label: int, value: int):
+    liblo.send(target, "/hw_controller", label, value)
+    return None
 
-while True:
-    try:
-        datum = arduino.read(2)
-        packed = int.from_bytes(datum, byteorder='big')
-        label = packed >> 8
-        value = packed & 255
-        print(f"controller {label}: {value}")
-        time.sleep(0.01)
-    except KeyboardInterrupt:
-        exit()
+def decode_bytes(packed): 
+    label = packed >> 8
+    value = packed & 255
+    return label, value
+
+def main():
+    arduino = serial.Serial('/dev/ttyS0', 9600, timeout=None)
+    target_address = liblo.Address(9000)
+    previous_val = 0
+
+    while True:
+        try:
+            datum = int.from_bytes(arduino.read(2), byteorder='big')
+            if datum != previous_val:
+                label, value = decode_bytes(datum)
+                #print(f"controller {label}: {value}")
+                send_msg(target_address, label, value)
+                previous_val = datum 
+            time.sleep(0.01)
+        except KeyboardInterrupt:
+            exit()
+
+if __name__ == "__main__":
+    main()
