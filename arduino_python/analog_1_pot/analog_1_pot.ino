1029936649110e8a5af95a6aba98ffd298401973
franeum desk <elettrochuck@gmail.com>
2020-10-25 20:08:23 +0100

    boh

--- arduino_python/analog_1_pot/analog_1_pot.ino
+++ arduino_python/analog_1_pot/analog_1_pot.ino
@@ -1,14 +1,13 @@
 #include <ResponsiveAnalogRead.h>
 
-const int SIZE = 3;
-const int ANALOG_PIN[] = { A0, A1, A2 };
-byte d[] = { 0,0,0 };
+const int ANALOG_PIN = A0;
+//byte d[] = { 0,0 };
 
 // make a ResponsiveAnalogRead object, pass in the pin, and either true or false depending on if you want sleep enabled
 // enabling sleep will cause values to take less time to stop changing and potentially stop changing more abruptly,
 //   where as disabling sleep will cause values to ease into their correct position smoothly and more accurately
 // ResponsiveAnalogRead analog(ANALOG_PIN, true);
-ResponsiveAnalogRead *analog_reads[SIZE];
+ResponsiveAnalogRead analog_pin(ANALOG_PIN, true);
 
 // the next optional argument is snapMultiplier, which is set to 0.01 by default
 // you can pass it a value from 0 to 1 that controls the amount of easing
@@ -18,37 +17,39 @@ ResponsiveAnalogRead *analog_reads[SIZE];
 void setup() {
   // begin serial so we can see analog read values through the serial monitor
   Serial.begin(115200);
-  //analog.setAnalogResolution(1024);
-  for (int i=0; i<SIZE; i++) {
-    analog_reads[i] = new ResponsiveAnalogRead(ANALOG_PIN[i], true);
-  }
+  //analog_pin = new ResponsiveAnalogRead(ANALOG_PIN, true);
 }
 
 void loop() {
   // update the ResponsiveAnalogRead object every loop
-  for (int i=0; i<SIZE; i++) {
-    analog_reads[i]->update();
-  
-    // if the repsonsive value has change, print out 'changed'
-    if(analog_reads[i]->hasChanged()) {
-      uint32_t value = analog_reads[i]->getValue();
-      get_bytes(d, (byte)i, value);
-    }
+  analog_pin.update();
+
+  if (analog_pin.hasChanged()) {
+    uint32_t value = analog_pin.getValue();
+    //get_bytes(d, value);
+    byte val = byte(value >> 2);
+    Serial.write(val);
   }
   
   delay(10);
 }
 
-void get_bytes(byte x[], byte label, uint32_t number) {
-  number = number >> 2;
+void get_bytes(byte x[], uint32_t number) {
+  //number = number >> 2;
   //Serial.println((String)number);
+  byte val = byte(number >> 2);
+  x[0] = 1;
+  x[1] = val;
+  Serial.print(x[0]);
+  Serial.print("\t");
+  Serial.println(x[1]);
+  /*
   byte dx = (byte)(number & 255);
-  byte sx = (byte)((number >> 8) & 255); 
-  //sx = sx + (label << 4);
-  x[2] = dx;
-  x[1] = sx;
-  x[0] = label;
-  Serial.write(x, 3);
+  byte sx = (byte)((number >> 8) & 15); 
+  sx = sx + (label << 4);
+  x[1] = dx;
+  x[0] = sx;
+  Serial.write(x, 2);*/
   /*
   Serial.print(x[0]);
   Serial.print(",");
