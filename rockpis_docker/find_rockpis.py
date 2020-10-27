2aa97c9e658035e66c81d663d86e0ef21e6f5a79
neum <neum@debian-top.myhome>
2020-10-27 23:36:54 +0100

    add rockpis ip finder

---
+++ rockpis_docker/find_rockpis.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import socket
+import json
+import sys 
+
+
+PORT = 37020
+broadcast = '<broadcast>'
+
+
+def set_broadcast_address(arg):
+    if arg == 'dev':
+        b = '192.168.4.255'
+    else:
+        b = '<broadcast>'
+
+    return b
+
+
+def create_socket():
+    client = socket.socket(socket.AF_INET, 
+                            socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.settimeout(0.5)
+    return client 
+
+
+def send_code(cl, broadcast, port):
+    cl.sendto(b'1', (broadcast, port))
+    return None 
+
+
+def get_response(cl):
+    while True:
+        try:
+            data, addr = client.recvfrom(1024)
+            data = json.loads(data.decode('utf-8'))
+            machine = data.get('machine', None)
+
+            if machine and machine == 'rockpis':
+                print(addr[0])
+                client.close()
+                exit(0)
+        except KeyboardInterrupt:
+            print("chiudo mestamente")
+            client.close()
+            exit(1)
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    broadcast = set_broadcast_address(arg)
+
+    while True:
+        try:
+            client = create_socket()
+            send_code(client, broadcast, PORT)
+            get_response(client)
+        except socket.timeout:
+            continue 
---
+++ rockpis_docker/send_ip_from_rockpis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import socket
+import json
+
+
+PORT = 37020
+message = { "machine": "rockpis" }
+
+
+def create_socket():
+    client = socket.socket(socket.AF_INET, 
+                            socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.bind(("", PORT))
+    return client 
+
+
+def send_response(cl):
+    while True:
+        try:
+            data, addr = cl.recvfrom(1024)
+            data = data.decode('utf-8')
+            if data and data == '1':
+                msg = json.dumps(message)
+                cl.sendto(msg.encode(), (addr[0], addr[1]))
+                cl.close()
+                print("chiudo correttamente")
+                exit(0)
+        except KeyboardInterrupt:
+            cl.close()
+            print("key interrupt")
+            exit(0)
+
+
+if __name__ == "__main__":
+    client = create_socket()
+    send_response(client)
