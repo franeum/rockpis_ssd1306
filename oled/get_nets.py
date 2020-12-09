e89f0a99b0621081eb27480adf5ffa9c43f244f4
franeum <elettrochuck@gmail.com>
2020-12-09 21:03:08 +0100

    delete print()

--- oled/get_nets.py
+++ oled/get_nets.py
@@ -5,8 +5,6 @@ import subprocess
 def get_wifi_list():
     CMD = ['nmcli','-g','SSID','dev','wifi']
     wifi_list = subprocess.check_output(CMD).decode("utf-8")
-    print(wifi_list)
     wifi_list = [x.strip(None) for x in wifi_list.splitlines() if x != '']
     return list(set(wifi_list))
 
-print(get_wifi_list())
