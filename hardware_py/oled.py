533fc6436162ea30eb2762ff9e686c1329699f43
franeum <elettrochuck@gmail.com>
2020-12-12 00:47:20 +0100

    add oled main class

--- hardware_py/oled.py
+++ hardware_py/oled.py
@@ -30,59 +30,58 @@ point   C               space           2               n.of chars
 20      (12, 17)        (12, 17)        (12, 17)        10
 """
 
-
-# Create the I2C interface.
-i2c = busio.I2C(SCL3, SDA3)
-
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-
-# Clear display.
-disp.fill(0)
-disp.show()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new("1", (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2 
-TOP = padding
-bottom = height - padding
-# Move left to right keeping track of the current x position for drawing shapes.
+TOP = -2
 X = 0
 
+class OledDisplay:
+    def __init__(self, scl=board.SCL3, sda=board.SDA3, pixelX=128, pixelY=32):
+        self.i2c = busio.I2C(scl, sda) 
+        self.disp = adafruit_ssd1306.SSD1306_I2C(pixelX, pixelY, self.i2c)
+        self.width = self.disp.width
+        self.height = self.disp.height
+        self._font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", size=12) 
+        self.__image()
+        self.__draw()
+        self.clear()
+    
+    def __image(self):
+        self.image = Image.new("1", (self.width, self.height))
 
-# Load default font.
-font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", size=12)
-#font = ImageFont.load_default()
+    def __draw(self):
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
+        # Draw a black filled box to clear the image.
+        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
 
+    def clear(self):
+        self.disp.fill(0)
+        self.show()
 
-def write_text(txt):
-    print(font.getlength(txt))
-    draw.text((X, TOP + 8), txt, font=font, fill=255)
-    disp.image(image)
-    disp.show()
+    def write_text(self, txt):
+        self.draw.text((X, TOP + 8), txt, font=self._font, fill=255)
+        self.disp.image(self.image)
+        self.disp.show()
 
+    def create_scrolling_text(self, string, a_time=0.1):
+        # 15
+        for i,ch in enumerate(string):
+            c = i + 1
+            self.write_text(string[:c])
+            time.sleep(a_time)
 
-def create_scrolling_text(string, a_time=0.1):
-    # 15
-    for i,ch in enumerate(string):
-        c = i+1
-        write_text(string[:c])
-        time.sleep(a_time)
+    @property
+    def font(self):
+        return self._font 
+    
+    @font.setter
+    def font(self, fontpath, size):
+        self._font = ImageFont.truetype(fontpath, size=size)
 
 
 if __name__ == "__main__":
-    write_text("PROVA PROVA 123")
+    o = OledDisplay()
+    o.write_text("TEST")
+    time.sleep(2)
+    o.create_scrolling_text("Scrolling........")
+    time.sleep(2)
+    o.clear()
