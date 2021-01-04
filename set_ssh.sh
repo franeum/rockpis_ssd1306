be9adb07a30264f86c4b3d5509161d05ae443546
franeum <elettrochuck@gmail.com>
2021-01-04 10:01:23 +0100

    final test

--- set_ssh.sh
+++ set_ssh.sh
@@ -4,6 +4,7 @@ for i in ROCKPIS-*; do
     cd ${i}
     name=`git remote -v | grep push | grep -Eo "franeum/[^/].+\.git"`
     #echo ${name}
+    git checkout main
     git remote set-url origin git@github.com:${name}
     cd ..
 done
