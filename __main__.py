812be7df4933a7efe33ef9b7e399188cb56d9a99
franeum desk <elettrochuck@gmail.com>
2021-01-08 18:52:49 +0100

    add 2 line of info on obect display and image

--- __main__.py
+++ __main__.py
@@ -13,16 +13,17 @@ from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
 
-#def write_message()
-
-
 # Create the I2C interface.
 i2c = busio.I2C(SCL3, SDA3)
+
+# oggetto display: è quello in cui vengono proiettate le immagini
+# oggetto image: crea le immagini da proiettare
+
 
 # Create the SSD1306 OLED class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # Clear display.
 disp.fill(0)
