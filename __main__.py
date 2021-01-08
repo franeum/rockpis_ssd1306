15e908ec3b83511c5e3a4d1841db72afc2b41262
franeum <elettrochuck@gmail.com>
2021-01-08 18:20:07 +0100

    add while test

--- __main__.py
+++ __main__.py
@@ -57,35 +57,27 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-R
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 
-"""
-while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    # Write four lines of text.
-    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 8), CPU, font=font, fill=255)
-    draw.text((x, top + 16), MemUsage, font=font, fill=255)
-    draw.text((x, top + 25), Disk, font=font, fill=255)
-    # Display image.
-    disp.image(image)
-    disp.show()
-    time.sleep(0.1)
-"""
-#while True:
 draw.text((x, top + 8), "Ciao Amore", font=font, fill=255)
 draw.text((x, top + 16), "grazie di esistere!", font=font, fill=255)
 disp.image(image)
 disp.show()
-time.sleep(10)
+time.sleep(2)
+disp.fill(0)
+disp.show()
+
+import keyword
+import random
+
+for _ in range(25):
+    parola = random.choice(keyword.kwlist)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    disp.fill(0)
+    disp.show()
+    draw.text((x, top + 8), parola, font=font, fill=255)
+    disp.image(image)
+    disp.show()
+    time.sleep(0.25)
+
 disp.fill(0)
 disp.show()
