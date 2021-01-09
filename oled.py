47d2f8d6ea4b0f9b28920f0e05e34158ff55000a
franeum <elettrochuck@gmail.com>
2021-01-09 17:16:49 +0100

    fix some errors

--- oled.py
+++ oled.py
@@ -8,12 +8,15 @@ WIDTH   = 128
 HEIGHT  = 64
 I2C     = busio.I2C(SCL3, SDA3)
 OLED    = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, I2C)
-IMAGE   = Image.new("1", (width, height))
-FONT    = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", size=10)
+
+
 
 class Oled64:
     def __init__(self):
-        self.draw = ImageDraw.Draw(IMAGE)
+        #self.oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, I2C)
+        self.image = Image.new("1", (WIDTH, HEIGHT))
+        self.font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", size=10)
+        self.draw = ImageDraw.Draw(self.image)
 
     def write(self, txt=None, row=1, newline=True):
         
@@ -21,7 +24,9 @@ class Oled64:
             self.clear_rect()
 
         line = row * (row - 1) * int(HEIGHT / 4)
-        self.draw.text((WIDTH, 0 + line), txt, font=font, fill=255)
+        self.draw.text((0, 0 + line), txt, font=self.font, fill=255)
+        OLED.image(self.image)
+        OLED.show()
 
     def clear_rect(self):
-        self.draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        self.draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)
