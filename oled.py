e7b6dae17eadbdb03b9dacbad27821248509f321
franeum desk <elettrochuck@gmail.com>
2021-01-09 16:21:05 +0100

    oled class added

--- oled.py
+++ oled.py
@@ -8,9 +8,20 @@ WIDTH   = 128
 HEIGHT  = 64
 I2C     = busio.I2C(SCL3, SDA3)
 OLED    = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, I2C)
-image   = Image.new("1", (width, height))
-
+IMAGE   = Image.new("1", (width, height))
+FONT    = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", size=10)
 
 class Oled64:
     def __init__(self):
-        pass 
+        self.draw = ImageDraw.Draw(IMAGE)
+
+    def write(self, txt=None, row=1, newline=True):
+        
+        if newline:
+            self.clear_rect()
+
+        line = row * (row - 1) * int(HEIGHT / 4)
+        self.draw.text((WIDTH, 0 + line), txt, font=font, fill=255)
+
+    def clear_rect(self):
+        self.draw.rectangle((0, 0, width, height), outline=0, fill=0)
